4e1e7cd8197ec60c9e9a9ecb188a5b46b21f05e9
May.may <studygit.github@gmail.com>
2020-11-24 14:36:07 +0000

    完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,13 @@
 class TasksController < ApplicationController
 
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
 
@@ -12,19 +16,20 @@ class TasksController < ApplicationController
 
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.new
   end
 
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクを作成しました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクを作成できませんでした'
-      render :new
+      render 'tasks/tasks'
     end
   end
 
@@ -46,16 +51,18 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-
     flash[:success] = 'タスクを削除しました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
 
 
   private
 
-  def set_task
-    @task = Task.find(params[:id])
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
   # Strong Parameter
